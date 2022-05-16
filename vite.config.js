ac7448dc44fb796ee0cb3d7b5f8aa695dcdda205
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-05-16 12:30:34 -0600

    rerenamed to docs cause pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: './build',
+    outDir: './docs/',
     sourcemap: false
   },
   server: {
